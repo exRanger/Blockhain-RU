15289337a0d686e5496128ffd8492f565ab8209e
Ilgiz <lmnasxzz@gmail.com>
2021-11-30 21:12:14 +0300

    Update Navigation.js

--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -1,31 +1,33 @@
-import 'antd/dist/antd.css';
-import './index.css';
 import { Menu } from 'antd';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import {useState} from 'react
+import 'antd/dist/antd.css';
+import './index.css';
 
 const { SubMenu } = Menu;
 
 export default function(){
+  
+  [item, setItem] = useState(null)
+  
   handleClick = e => {
-    console.log('click ', e);
+    // Link to?
   };
+  
+  
 
     return (
       <Menu
         onClick={this.handleClick}
-        style={{ width: 256 }}
+        style={{ width: 311 }}
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
         mode="inline"
       >
-        <SubMenu key="sub1" icon={<MailOutlined />} title="Навигация">
-          <Menu.ItemGroup key="g1" title="Рубрики">
-            <Menu.Item key="1">Option 1</Menu.Item>
-            <Menu.Item key="2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup key="g2" title="Item 2">
-            <Menu.Item key="3">Option 3</Menu.Item>
-            <Menu.Item key="4">Option 4</Menu.Item>
+        <SubMenu key="sub1" icon={<MailOutlined />} title="Nav">
+          <Menu.ItemGroup key="g1" title="Chapter one">
+            <Menu.Item key="1">Introdution to blockhain</Menu.Item>
+            <Menu.Item key="2">Installation</Menu.Item>
           </Menu.ItemGroup>
         </SubMenu>
         
