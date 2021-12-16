6e69d91c458e17a609f0b6291a17d152930ea3a9
Ilgiz <lmnasxzz@gmail.com>
2021-12-16 12:42:16 +0300

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,99 +1,53 @@
-const math = require('mathjs')
+import math from 'math.js'
+import {struct, u32, ns64} from "@solana/buffer-layout"
+import {Buffer} from 'buffer'
+import web3 from "@solana/web3.js"
 
-let x = 0.3/(9.4/24)**5
-console.log('x', x)
-const h = 1.06* 10**-27
-let w2 = 743
-let w = 24*10**9
-let rad = 6.28 
-let temperature = 4.2 // K 
-let kT_4K = 2*1.138*4.2*10**-16 
-let kt_05K =2*1.138*0.5*10**-16
+let keypair = web3.Keypair.generate();
+let payer = web3.Keypair.generate();
 
+let connection = new web3.Connection(web3.clusterApiUrl('testnet'));
 
-let division_4K = h*w*rad/kT_4K
-let division_05K =h*w*rad/kt_05K
+let allocateTransaction = new web3.Transaction({
+    feePayer: payer.publicKey
+  });
 
+let keys = [{pubkey: keypair.publicKey, isSigner: true, isWritable: true}];
+let params = { space: 250 };
 
-let right_e_4K = 6.3*10**9*Math.E**(-34/4.2)
-let right_e_05 = 6.3*10**9*Math.E**(-34/0.5)
+let airdropSignature = await connection.requestAirdrop(
+  payer.publicKey,
+  web3.LAMPORTS_PER_SOL,
+);
 
-let tensix = 10**6
-let T1e, T1e2;
+await connection.confirmTransaction(airdropSignature);
 
+let allocateTransaction = new web3.Transaction({
+  feePayer: payer.publicKey
+});
 
+let keys = [{pubkey: keypair.publicKey, isSigner: true, isWritable: true}];
+let params = { space: 100 };
 
-let result = x * math.coth(division_4K) + right_e_4K // 4.2K
-let div_T1e = result/tensix
-console.log('1/T1e',div_T1e)
+let allocateStruct = {
+  index: 8,
+  layout: struct([
+    u32('instruction'),
+    ns64('space'),
+  ])
+};
 
-T1e = 1/div_T1e
-console.log('T1e',T1e )
+let data = Buffer.alloc(allocateStruct.layout.span);
+let layoutFields = Object.assign({instruction: allocateStruct.index}, params);
 
+allocateStruct.layout.encode(layoutFields, data);
 
-let result2 = x * math.coth(division_05K) + right_e_05 // 0.5K
-let div_T2e = result2
+allocateTransaction.add(new web3.TransactionInstruction({
+  keys,
+  programId: web3.SystemProgram.programId,
+  data,
+}));
 
-
-T2e = 1/div_T2e
- 
-console.log('1/T2e', div_T2e)
-console.log('T2e', T2e)
-
-
-let one_div_tn = (8*3.14)/5*(1/4)*((1.6/(18.63*10**3))**2)*(2.37*10**19)/(5.68*10**22)*(2.576*10**6)/(4.26*10**3)*1.92*10**6
-
-let one_div_tn2 = (8*3.14)/5*(1/4)*((1.6/(18.63*10**3))**2)*(2.37*10**19)/(5.68*10**22)*(2.576*10**6)/(4.26*10**3)*36.72
-
-console.log('1/T1n 4K', one_div_tn*10**3)
-
-let T1n = 1/(4.48*10**-3)
-
-
-console.log('T1n', 1/(4.48*10**-3))
-
-
-console.log('T1e 0.5', one_div_tn2)
-console.log('1/T1e 0.5', 1/(one_div_tn2))
-
-
-let V = 250.63*10**3
-
-let S_4K = V* T1e * 10**-6
-let S_05 = V* T2e 
-console.log('S при 4K', S_4K)
-console.log('S при 0.5K', S_05)
-
-let Po_4k = math.tan(division_4K)
-let Po_05k= math.tan(division_05K)
-console.log('Po_4k', Po_4k )
-console.log('Po_0.5k', Po_05k)
-
-let Pns_4k = S_4K * Po_4k / (1+S_4K-Po_4k)
-let Pns_05k = S_05 * Po_05k / (1+S_05-Po_05k) 
-
-console.log('Pns_4k', Pns_4k)
-console.log('Pns_05k', Pns_05k)
-
-let divTau_4K = (1+S_4K-Po_4k) / T1n
-
-let divTau_05 = (1+S_05-Po_05k) /  (1/(one_div_tn2))
-
-console.log('1/Tau: 4.2K', divTau_4K)
-console.log('1/Tau: 4.2K', divTau_05)
-
-//Ce
+await web3.sendAndConfirmTransaction(connection, allocateTransaction, [payer, keypair]);
 
  
-let T = 4.2 
-
-
-let onedivT1e = w2 * math.coth((h*6.28*35*10**9*10**16)/4.4*1.38) + right_e_4K
-
-console.log('Ce4K', onedivT1e)
-
-let onedivT2e = w2 * math.coth((h*6.28*35*10**9*10**16)/2*1.38) + right_e_05
-
-console.log('Pn', 0.29/(1+0.13+2.84*10**-5-0.16**2))
-
-console.log('pn2', (1+6823+(0.345*10**-3)-5.94**2)/30*10**4)
