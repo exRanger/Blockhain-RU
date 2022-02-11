7abb55731f1e4bc0a55623e0a7d86ac9893127ce
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-02-11 14:07:46 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ const schema = require('./schema/schema')
 const {log} = console
 
 const uri // from .env ....
-
 const app = express()
 const PORT = 3030 // .env
 
@@ -18,16 +17,14 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 app.get('/nocors', (req, res) => {
-  res.send('ok');
+  res.send('ok')
 });
 
-app.use(require('cors')());
+app.use(require('cors')())
 
-// This response will have CORS headers, because this route handler
-// is after the CORS middleware in the middleware list.
 app.get('/cors', (req, res) => {
-  res.send('ok');
-});
+  res.send('ok')
+})
 
 app.listen(PORT, err => {
     err ? log(error) : log(`Server listen on ${PORT} port`)
