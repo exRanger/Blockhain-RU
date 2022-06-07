8b396b26d07ab134fd12d4e38edb9ff584ceb379
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-06-07 22:10:05 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,31 +1,18 @@
 const express = require('express') // or nest?
-const {graphqlHTTP} = require('express-graphql')
-const {mongoose} = require('mongoose')
 const fs = require('fs')
 const path = require('path')
-const schema = require('./schema/schema')
+const SHA256 = require('crypto-js/sha256')
 
-const {log} = console
-
-const uri // from .env ....
-const app = express()
-const PORT = 3030 // .env
-
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}))
-
-app.get('/nocors', (req, res) => {
-  res.send('ok')
+class BlockCypto {
+    constructor(index, current_time, info, nextHash=" "){
+        this.index = index
+        this.current_time = current_time
+        this.info = info
+        this.nextHash = nextHash
+        this.hash = this.computeHash()  
+    }
+    computeHash(){
+        return SHA256(this.info + this.nextHash + this.current_time + JSON.stringify(this.info)).toString();
+    }   
 })
 
-app.use(require('cors')())
-
-app.get('/cors', (req, res) => {
-  res.send('ok')
-})
-
-app.listen(PORT, err => {
-    err ? log(error) : log(`Server listen on ${PORT} port`)
-})
