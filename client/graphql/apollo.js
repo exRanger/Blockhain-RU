e66681aea55cc1b29d2231558fbd82f56b15e8e9
Zagirov Il'giz <lmnasxzz@gmail.com>
2023-10-09 16:30:00 +0300

    Update apollo.js

--- client/graphql/apollo.js
+++ client/graphql/apollo.js
@@ -1,11 +1,12 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client"
 
 const client = new ApolloClient({
-    uri: "https://gql-2.test.serafim.help/v1/graphql",
-    cache: new InMemoryCache(),
-    headers: {
-        'x-hasura-admin-secret':'123-123-123-123-123'
-    }
-})
+  uri: '',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: localStorage.getItem('token'),
+    'client-name': 'WidgetX Ecom [web]',
+    'client-version': '1.0.0'
+  }
 
 export default client
