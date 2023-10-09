4011ccba9f05cefd416289637cc280a30b167fa5
Zagirov Il'giz <lmnasxzz@gmail.com>
2023-10-09 16:31:01 +0300

    Update apollo.js

--- client/graphql/apollo.js
+++ client/graphql/apollo.js
@@ -1,4 +1,4 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client"
+import {ApolloClient, ApolloLink, InMemoryCache} from "@apollo/client"
 
 const client = new ApolloClient({
   uri: '',
@@ -8,6 +8,15 @@ const client = new ApolloClient({
     'client-name': 'WidgetX Ecom [web]',
     'client-version': '1.0.0'
   }
+});
+
+const formatDateLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map(response => {
+    if (response.data.date) {
+      response.data.date = new Date(response.data.date);
+    }
+    return response;
+  });
 });
 
 export default client
