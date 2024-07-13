ecaa8a30ee19aa2c67160b61659109f96c34773f
Zagirov Il'giz <lmnasxzz@gmail.com>
2024-07-13 23:40:39 +0300

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -40,16 +40,7 @@ contract Ballot {
 
   
     function giveRightToVote(address voter) external {
-        // If the first argument of `require` evaluates
-        // to `false`, execution terminates and all
-        // changes to the state and to Ether balances
-        // are reverted.
-        // This used to consume all gas in old EVM versions, but
-        // not anymore.
-        // It is often a good idea to use `require` to check if
-        // functions are called correctly.
-        // As a second argument, you can also provide an
-        // explanation about what went wrong.
+   
         require(
             msg.sender == chairperson,
             "Only chairperson can give right to vote."
