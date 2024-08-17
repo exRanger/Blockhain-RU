261c0051b4da43ae3cb8f49208b4de93871a5412
Zagirov Il'giz <lmnasxzz@gmail.com>
2024-08-17 23:20:23 +0300

    Update CallContract.js

--- server/CallContract.js
+++ server/CallContract.js
@@ -1,21 +1,25 @@
 const MyContract = artifacts.require('MyContract');
-const {log} = console;
+const {log, error} = console;
+
+const loggerError = (error) => {
+  error('CATHCHED', error);
+}
 
 module.exports = async function (callback) {
   try {
     const accounts = await web3.eth.getAccounts();
     const myContract = await MyContract.deployed();
     const contractOwner=await myContract.owner();
-    console.log('Contract owner:', contractOwner);
+    log('Contract owner:', contractOwner);
     // Call a function on the contract
     const result = await myContract.Call();
-    console.log('Result:', result);
+    log('Result:', result);
     // Send a transaction to the contract
     const tx = await myContract.Call({ from: accounts[0], value: web3.utils.toWei('1', 'ether') });
-    console.log('Transaction:', tx);
+    log('Transaction:', tx);
     callback();
   } catch (error) {
-    console.error(error);
+    loggerError(error)
     callback(error);
   }
 };
